53280e9eb45570958a8fd0ae9aef4b6c25210312
Renuka Panigrahi <panigrahirenuka599@gmail.com>
2019-04-05 22:38:20 -0400

    Register Component, Validation & Flash error message using angular

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import {RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -8,14 +11,15 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ValidateService  } from './services/validate.service';
+import {FlashMessagesModule} from 'angular2-flash-messages/module';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'profile', component: ProfileComponent},
-   
+const appRoutes: Routes =  [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'profile', component: ProfileComponent}
 ]
 
 @NgModule({
@@ -29,11 +33,14 @@ const appRoutes: Routes = [
     ProfileComponent
   ],
   imports: [
-    BrowserModule, 
-    RouterModule.forRoot(appRoutes)
-
+    BrowserModule,
+    FormsModule,
+    
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
+    
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
