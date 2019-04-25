db3c7e14a1aafa5081621ce705148693c11b6b43
Renuka Panigrahi <panigrahirenuka599@gmail.com>
2019-04-25 15:19:11 -0400

    server changes for user component to work with login register

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -11,11 +11,14 @@ function isEmptyObject(obj) {
 exports.addNewUser = function (req, res) {
     const email = req.body.email;   
     let newUser = new User(req.body);
+    console.log("addNewUser"+ email);
     User.addUser(newUser, (err, user) => {
-        if (err) {            
+        if (err) {   
+            console.log("addNewUser"+ email);
             res.json({ success: false, msg: 'Failed to register user' });
-        } else {           
-            res.status(201).send({ id_token: createToken(email) });
+        } else {      
+            console.log("addNewUser else "+ email);     
+            return res.status(201).send({ id_token: createToken(email) });
         }
     });
 };
@@ -36,20 +39,17 @@ exports.authenticateUser = function (req, res) {
     User.getUserByEmail(email, (err, user) => {
         if (err) throw err;
         if (!user) {
-            return res.json({ success: false, msg: 'User not found' });
             console.log("User Not Found");
+            return res.json({ success: false, msg: 'User not found' });            
         }
         User.comparePassword(password, user.password, (err, isMatch) => {
             if (err) throw err;
             if (isMatch) {
-                console.log("Password matched");
-
-                //const token = jwt.sign(_.omit(user.email, 'password'), config.secret, { expiresInMinutes: 60*5 });
-                //const token = jwt.sign({ data: user }, config.secret, { expiresIn: 604800 });
-                //console.log("User Found returnig the token -- "+ token);
-                //return res.status(200).json({"token": token});
-                //res.status(201).send({ id_token: token});
-                res.status(201).send({ id_token: createToken(email) });
+                console.log("Password matched"+ email)
+                res.status(201).send({ 
+                                        id_token: createToken(email),
+                                        user:_.pick(user,['fullName', 'email', 'username', 'height', 'weight', 'gender', 'address'])}
+                                    );
             } else {
                 console.log("Password not matched");
                 return res.json(404).json("password not matching");
@@ -89,19 +89,22 @@ exports.removeUserDetails = function (req, res) {
 };
 
 exports.getUserProfileByUsername = function (req, res) {
-    const email = "re@gmail.com";
-   console.log("getUserProfileByUsername() email--"+ email);
-   User.getUserByEmail(email,(err, user) => {
+    const email = req.query.email;
+    
+    console.log("email-- "+ email);
+    User.getUserByEmail(email,(err, user) => {
        console.log("getUserProfileByUsername() user--"+ user);
        if(err) throw err;
        if(!user)
        return res.status(404).json({status: false, message: "User record not found"});
        else
        return res.status(200).json({status: true, user:_.pick(user,['fullName', 'email', 'username', 'height', 'weight', 'gender', 'address'])});
-   });
+    });
 
 };
 
 function createToken(user) {
-    return jwt.sign(_.omit(user, 'password'), config.secret, { expiresInMinutes: 60*5 });
+    const token = jwt.sign(_.omit(user, 'password'), config.secret, { expiresInMinutes: 60*5 });
+    console.log("token in the server for user "+ token);
+    return token;
   }
